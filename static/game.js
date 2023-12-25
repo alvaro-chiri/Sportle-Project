f0c8abbf575704af7f7fbdcaf39eaf8241f92f24
alvaro-chiri <65077832+alvaro-chiri@users.noreply.github.com>
2023-12-25 11:33:17 -0700

    game.js needed to go into static folder, changed the way the gameboard was rendered in play.html and in styles.css

--- static/game.js
+++ static/game.js
@@ -6,17 +6,19 @@ var col = 0;
 
 var gameOver = false
 
-window.onload = function(){
+document.addEventListener('DOMContentLoaded', function () {
     initialize();
-}
+});
+
 
 function initialize() {
     // make gameboard
     for (let r = 0; r < height; r++) {
-        for (let c = 0; c< width; c++) {
+        for (let c = 0; c < width; c++) {
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
+            tile.innerText = " ";
             document.getElementById("board").appendChild(tile);
         }
     }
